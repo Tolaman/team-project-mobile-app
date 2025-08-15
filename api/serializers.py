a00951b985a28cc1a3da065d8093cf642750186d
Michee Luzayisu <michemakaya11@gmail.com>
2025-08-15 14:38:35 +0200

    create the user serializer and the report by importing the report model

---
+++ api/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import report
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['id', 'username', 'email', 'password']
+    extra_kwargs = {'password': {'write_only': True}}
+
+  def create(self, validated_data):
+    user = user.objects.create_user(**validated_data)
+    return user
+  
+class ReportSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = report
+    fields = ['id', 'full_name', 'gender', 'location', 'condition', 'date', 'user']
+    extra_kwargs = {
+      'user': {'read_only': True}
+    }
