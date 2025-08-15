c07909bb0883511c7e4fcf08ceca9f28d75f11db
Michee Luzayisu <michemakaya11@gmail.com>
2025-08-15 17:30:10 +0200

    add the report and shelter database to the admin platform

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Report, Shelter
+#from django.contrib.gis import admin as gis_admin
 
 # Register your models here.
+admin.site.register(Report)
+admin.site.register(Shelter)
