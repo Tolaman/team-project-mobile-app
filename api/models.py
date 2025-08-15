cd3325c8ce02126fd250f831c674ae9eeec56541
Michee Luzayisu <michemakaya11@gmail.com>
2025-08-15 17:28:22 +0200

    created the model for the shelter

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class report(models.Model):
+class Report(models.Model):
   full_name = models.CharField(max_length=100)
   gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
   location = models.CharField(max_length=100)
@@ -11,3 +11,13 @@ class report(models.Model):
 
   def __str__(self):
     return self.full_name
+  
+class Shelter(models.Model):
+  name = models.CharField(max_length=100)
+  address = models.CharField(max_length=255)
+  phone_number = models.CharField(max_length=15)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name
