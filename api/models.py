9e17f190824ee7cac2a4c58c79508f4adee04d0a
Michee Luzayisu <michemakaya11@gmail.com>
2025-08-15 14:33:17 +0200

    created the report model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class report(models.Model):
+  full_name = models.CharField(max_length=100)
+  gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female')])
+  location = models.CharField(max_length=100)
+  condition = models.TextField()
+  date = models.DateField(auto_now_add=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reports')
+
+  def __str__(self):
+    return self.full_name
